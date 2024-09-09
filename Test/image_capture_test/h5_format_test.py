42ec68a6fd1c1b523fc45aa4923d0a1fe722d0f8
Ghimire <bishal.ghimire@siu.edu>
2024-09-09 16:32:56 -0500

    refactor

--- Test/image_capture_test/h5_format_test.py
+++ Test/image_capture_test/h5_format_test.py
@@ -2,9 +2,15 @@
 a test case to check if teh camera is able to capture 500 
 images and save it in single h5 file format
  """
+import time
 from flir_image_capture_package.flir_camera_parameters import FlirCamParam
 from flir_image_capture_package.flir_image_capture import FlirCamera
+start= time.time()
 cameraParam = FlirCamParam()
-cameraParam.snap_count = 500
+cameraParam.snap_count = 10
+
 camera =FlirCamera(cameraParam)
 camera.take_snapshot()
+end_time = time.time()
+elapsed_time = end_time - start
+print(f"Time taken to run the block of code: {elapsed_time} seconds")
