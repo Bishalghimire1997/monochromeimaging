d9126ebc6308eb0b411af39ef24e8956182c64ca
Ghimire <bishal.ghimire@siu.edu>
2024-09-16 13:18:16 -0500

    pulse simulation added

--- Test/image_capture_test/led_control_test.py
+++ Test/image_capture_test/led_control_test.py
@@ -1,9 +1,9 @@
 """ teste set for led_control class
     """
+import time
 import math
-from thors_lab_led_control_package.constant_brightness_mode import ConstantBrightness
 import pyvisa
-
+from thors_lab_led_control_package.constant_brightness_mode import ConstantBrightness
 rm = pyvisa.ResourceManager()
 resources = rm.list_resources()
 instrument = rm.open_resource(resources[0])
@@ -17,7 +17,20 @@ def sim_sine(obj: ConstantBrightness):
         perc = math.sin(math.radians(i))*100
         obj.set_led_brigntness(perc)
     obj.off()
+
+
+def sim_pulse(obj: ConstantBrightness):
+    """Simulates pulse 
+
+    Args:
+        obj (ConstantBrightness): instance of ConstantBrightness class 
+    """
+    obj.set_led_brigntness(100)
+    for i in range(10):
+        obj.on()
+        time.sleep(1)
+        obj.off()
+        time.sleep(1)
+        
 sim_sine(obj_cb)
-
-
-
+sim_pulse(obj_cb)
