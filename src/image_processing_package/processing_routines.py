326b2aa02c49433c1241ca08975be64abdc0de49
Ghimire <bishal.ghimire@siu.edu>
2024-10-01 12:24:13 -0500

    first working vesion of color correction

--- src/image_processing_package/processing_routines.py
+++ src/image_processing_package/processing_routines.py
@@ -80,13 +80,105 @@ class Processing():
     def color_correction (roi:tuple,refrence_color: list, image):
         x, y, w, h = roi
         cropped_image = image[y:y+h, x:x+w]
-        b_channel, g_channel, r_channel = cv2.split(cropped_image)
-        x=np.array(  np.array([np.mean(b_channel), np.mean(g_channel), np.mean(r_channel)]))
+        b_channel = cropped_image[:, :, 0]
+        g_channel = cropped_image[:, :, 1]
+        r_channel = cropped_image[:, :, 2]
+        b_channel_ref = np.full((h,w),refrence_color[0],dtype=np.uint8)
+        g_channel_ref=  np.full((h,w),refrence_color[1],dtype=np.uint8)
+        r_channel_ref = np.full((h,w),refrence_color[2],dtype=np.uint8)
 
+
+        W_blue, _, _, _ = np.linalg.lstsq(b_channel, b_channel_ref, rcond=None)
+        W_green, _, _, _ = np.linalg.lstsq(g_channel, g_channel_ref, rcond=None)
+        W_red, _, _, _ = np.linalg.lstsq(r_channel, r_channel_ref, rcond=None)
+
+        transformed_blue =np.dot(b_channel,W_blue)
+        transformed_green =np.dot(g_channel,W_green)
+        transformed_red =np.dot(r_channel,W_red)
+        transformed_image = np.stack((transformed_blue.astype(np.uint8),transformed_green.astype(np.uint8),transformed_red.astype(np.uint8)),axis=-1)
+
+        print(W_blue)
+        print("")
+        print(W_red)
+
+        print("")
+        print(W_green)
+        cv2.imshow("transformed image",transformed_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         
-        y = np.array(refrence_color)
-        print(x,y)
-        W, _, _, _ = np.linalg.lstsq(x.T, y, rcond=None)
-        print(w)
+        print(W_blue)
+        print("")
+        print(W_red)
+
+        print("")
+        print(W_green)
         
         pass
+    @staticmethod
+    def pixel_correction(roi:tuple,refrence_color: list, image):
+         x, y, w, h = roi
+         cropped_image = image[y:y+h, x:x+w]
+         b_pixel = np.mean(cropped_image[:, :, 0])
+         g_pixel = np.mean(cropped_image[:, :, 1])
+         r_pixel = np.mean(cropped_image[:, :, 2])
+
+         
+
+        # channel_arr = np.array([b_channel,g_channel,r_channel])
+
+         pixel_value = np.array([[b_pixel,g_pixel,r_pixel]])
+         print(pixel_value)
+         refrence = np.array([refrence_color])        
+
+         W, _, _, _ = np.linalg.lstsq(pixel_value, refrence, rcond=None)  
+         
+         return W       
+
+         
+
+    @staticmethod
+    def fit(image, weight):
+          image_reshaped = image.reshape(-1, 3)
+          transformed_image = np.dot(image_reshaped, weight.T)
+          transformed_image = np.clip(transformed_image, 0, 255)
+          transformed_image = transformed_image.reshape(len(image), len(image[0]), 3)
+          return transformed_image.astype(np.uint8)
+    @staticmethod
+    def fit_colors(wb,wg,wr,colored_image):
+        blue_fit = Processing.fit(colored_image,wb)
+        green_fit = Processing.fit(colored_image,wg)
+        red_fit = Processing.fit(colored_image,wr)
+        blue_reshape = blue_fit.reshape(-1,3)
+        green_reshape =  green_fit.reshape(-1,3)
+        red_reshape = red_fit.reshape(-1,3)
+        return  np.stack((blue_fit[:, :, 0],green_fit[:, :, 1],red_fit[:, :, 2]),axis = -1)#sum.reshape(len(colored_image), len(colored_image[0]), 3).astype(np.uint8) #np.stack((blue_fit[:, :, 0],green_fit[:, :, 1],red_fit[:, :, 2]),axis = -1)
+          
+    @staticmethod
+    def pixel_retio_correction(orginal_image, transformed_image):
+        ratio = orginal_image/255
+        ratio_rgb_channel = ratio.reshape(-1,3)
+        bgr_transformed = transformed_image.reshape(-1,3)
+        bgr_orginal= orginal_image.reshape(-1,3)
+        ref= bgr_orginal[0]
+        blue_ratio_trasformed = ((ref*ratio_rgb_channel[1]).astype(np.uint8))        
+        green_ratio_transformed = (ref*ratio_rgb_channel[1]).astype(np.uint8)
+        red_ratio_trasformed = ((ref*ratio_rgb_channel[1]).astype(np.uint8))        
+       
+
+
+
+       
+         
+         
+
+
+    
+
+
+        
+        
+
+
+
+
